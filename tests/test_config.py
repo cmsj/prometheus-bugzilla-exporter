556001dc4ef299a14502391323ccc727dae5f9ff
Chris Jones <cmsj@tenshu.net>
2018-11-23 14:15:21 +0000

    Add a test for a simple counter query

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,7 @@ import pytest
 
 from prombzex.prombzex import PromBZEx
 from prombzex.server import BZServer
+from simple_rest_client.models import Response
 
 
 def test_empty_config():
@@ -41,3 +42,31 @@ def test_one_server_no_queries():
         server = bz.server(server_name)
         assert bz.config_get('timeout', server_name) == '657483'
         assert server.query_names() == []
+
+
+def test_server_query_simple_counter(monkeypatch):
+    def mock_search_return(params=None):
+        assert params['test1'] == 'test2'
+        return Response(status_code=200,
+                        url='',
+                        method='GET',
+                        headers={},
+                        client_response=None,
+                        body={"bugs": [{}, {}]})
+
+    with open('tests/config-one-server-one-query.json', 'r') as filep:
+        bz = PromBZEx(filep)
+    assert isinstance(bz, PromBZEx)
+    assert len(bz.servers()) == 1
+    for server_name in bz.servers():
+        assert isinstance(server_name, str)
+        server = bz.server(server_name)
+        monkeypatch.setattr(server.bugzilla.bz, 'search', mock_search_return)
+        with pytest.raises(KeyError):
+            server.run_query('non-existant query')
+        for query_name in server.query_names():
+            output = server.run_query(query_name)
+            assert output == """# HELP simple_counter_test Help for simple_counter_test
+# TYPE simple_counter_test counter
+simple_counter_test 2
+"""
