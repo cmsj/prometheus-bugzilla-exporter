bdce6277466494c2b9eeba814e36edbc30e374f3
Chris Jones <cmsj@tenshu.net>
2018-12-11 17:03:49 +0000

    Following Python Packaging Guide

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(name='prombzex',
-      version='0.1',
-      description='Prometheus Bugzilla Exporter',
-      url='https://github.com/cmsj/prombzex',
-      author='Chris Jones',
-      author_email='cmsj@tenshu.net',
-      license='MIT',
-      packages=['prombzex'],
-      scripts=['bin/prombzex'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='prombzex',
+    version='0.1',
+    author='Chris Jones',
+    author_email='cmsj@tenshu.net',
+    description='Prometheus Bugzilla Exporter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/cmsj/prombzex',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved "" MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['bin/prombzex'],
+    zip_safe=False
+)
