21cf9efee4f6b906bf9113318d7a7d485fa9e361
Chris Jones <cmsj@tenshu.net>
2018-12-14 16:36:31 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='prombzex',
-    version='0.1',
+    version='0.2',
     author='Chris Jones',
     author_email='cmsj@tenshu.net',
     description='Prometheus Bugzilla Exporter',
